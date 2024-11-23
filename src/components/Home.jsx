ff40cd7ebda156d4278771bc5684d461311f6e9b
Maycol Zincker <zincker2000@gmail.com>
2024-11-23 02:40:59 -0300

    Update styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,39 @@ import './../style/home.css'
 function Home() {
   
     return (
-        <div className="hero">
-            <div className="cuadros">
-                <div className='cuad'>cuadro 1</div>
-                <div className='cuad'>cuadro 2</div>
-                <div className='cuad'>cuadro 3</div>
-                <div className='cuad'>cuadro 4</div>
-                <div className='cuad'>cuadro 5</div>
+        <div className="homepage">
+            <div className="hero">
+                <div className="titulo">
+                    <h1>Mi Librería</h1>
+                </div>
+                <div className="parrafo">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam amet ab, aut dicta dolorem minima voluptate ducimus eos ipsa cupiditate deserunt repellendus minus magni ipsam ratione repudiandae hic porro laboriosam? Illum, distinctio aut! Cum ad saepe, earum dicta at similique sunt modi consectetur. Fuga sapiente earum itaque eaque omnis? Ipsam.</p>
+                </div>
+            </div>
+            <div className="hero2">
+                <div className="hero2Izq">
+                    <div className="subtitulo">
+                        <h2>Muchos libros en esta librería</h2>
+                    </div>
+                    <div className="parrafo2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quasi atque suscipit possimus officiis ab quos reprehenderit velit delectus, inventore consectetur quas ratione corporis ipsam, vitae autem eum? Omnis, accusantium?</p>
+                    </div>
+                </div>
+                <div className="hero2Der">
+                    <div className="imagen">
+                        <h4>Aqui hay imagen</h4>
+                    </div>
+                    <div className="datos">
+                        <div className="dato1">
+                            <div>libro 1</div>
+                            <div>libro 2</div>
+                        </div>
+                        <div className="dato2">
+                            <div>libro 3</div>
+                            <div>libro 4</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import './../style/navbar.css';
+import logo from "./../assets/imagenLogo.jpg"
 
 function Navbar() {
     return (
         <div className='nav'>
-            <div className="logo">Logo</div>
+            <a href="https://www.google.com" className='logo-link'>
+                <img src={logo} alt="Logo de la empresa" className='logo'/>
+            </a>
+
+            <div className="medio">
+                <a href="https://www.google.com">Home</a>
+                <a href="https://www.google.com">About</a>
+                <a href="https://www.google.com">Books</a>
+            </div>
             <div className="links">
                 <a href="https://www.google.com">Sign In</a>
                 <a href="https://www.google.com">Sign In</a>
